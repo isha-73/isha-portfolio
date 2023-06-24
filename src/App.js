f73d0693c1168c7c231d86bca54e5bd148f80647
Isha Bule <ishabule753@gmail.com>
2023-06-25 00:55:01 +0530

    Drawer and header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import DrawerMenu from './components/DrawerMenu';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DrawerMenu className="drawer" />
       </header>
+      <body className='App-body'>
+        <p>Hello</p>
+        </body>
+      <footer className='App-footer'>
+        <p>Footer</p>
+      </footer>
     </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/components/DrawerMenu.js
@@ -0,0 +1,40 @@
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+  } from '@chakra-ui/react'
+import './DrawerMenu.css'
+import { PhoneIcon, HamburgerIcon } from '@chakra-ui/icons'
+import { Button, useDisclosure } from "@chakra-ui/react"
+
+export default function PlacementExample() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    
+
+    return (
+      <>
+      
+        <Button className='draw' backgroundColor='#282c34' height={'10vh'}  borderRadius={'0'} onClick={onOpen}variant="unstyled">
+          <HamburgerIcon color="red.500" boxSize={6} />
+        </Button>
+        <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay />
+          <DrawerContent backgroundColor={'black'}>
+            <DrawerHeader borderBottomWidth='1px' textAlign={'center'}color= "white" fontSize={'30px'}>Hello</DrawerHeader>
+            <DrawerBody className='drawerbody'>
+             <Button className='drawerbody-button' bg="black"  color= "white" _hover={{ bg: "red.500" }}>Home</Button>
+             <Button className='drawerbody-button' bg="black"color= "white" _hover={{ bg: "red.500", color: "white" }}>About Me</Button>
+             <Button className='drawerbody-button' bg="black"color= "white" _hover={{ bg: "red.500", color: "white" }}>Blogs</Button>
+            <Button className='drawerbody-button' bg="black" color= "white"_hover={{ bg: "red.500", color: "white" }}>Projects</Button>
+            <Button className='drawerbody-button' bg="black" color= "white"_hover={{ bg: "red.500", color: "white" }}>Timeline</Button>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </>
+    )
+  }
